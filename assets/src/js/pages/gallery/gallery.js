bf97b2ec18c2bc387e1d2eed4b0609c072f90a74
Stas Khitrich <staskhitrich@gmail.com>
2022-06-19 15:58:11 +0500

    Обновил стили галереи, хедера и структуру. Подключил слик слайдер

--- assets/src/js/pages/gallery/gallery.js
+++ assets/src/js/pages/gallery/gallery.js
@@ -1,1 +1,21 @@
 import 'SCSS/pages/gallery/gallery.scss';
+
+jQuery(window).on('load', function() {
+	jQuery('.section-gallery__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		adaptiveHeight: true,
+		asNavFor: '.section-gallery__slider-nav',
+	});
+
+	jQuery('.section-gallery__slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		centerMode: true,
+		focusOnSelect: true,
+		adaptiveHeight: true,
+		asNavFor: '.section-gallery__slider',
+	});
+})
